a2602ffad597f497066ac7065ae34abfd46c9823
Seth Meldon <smeldon@progress.com>
2025-03-21 14:08:42 -0400

    update history.js

--- clientSideComponent/dynForm/history.js
+++ clientSideComponent/dynForm/history.js
@@ -1,49 +1,51 @@
-corticon.util.namespace( "corticon.dynForm" );
+import { customEvents, raiseEvent } from './customEvents.js'; // Adjust the path if needed
 
-corticon.dynForm.History = function () {
-    let itsHistory = [];
-
-    function setupHistory() {
-        corticon.dynForm.addCustomEventHandler( corticon.dynForm.customEvents.AFTER_UI_STEP_RENDERED, storeDecisionServiceInputs2);
+export class History {
+    constructor() {
+        this.itsHistory = [];
     }
 
-    function storeDecisionServiceInputs2 ( event ) {
+    setupHistory() {
+        raiseEvent(customEvents.AFTER_UI_STEP_RENDERED, this.storeDecisionServiceInputs2.bind(this));
+    }
+
+    storeDecisionServiceInputs2(event) {
         const theData = event.theData;
-        // We do a deep Copy of the input.  We need it otherwise we get a reference to current input
+        // We do a deep Copy of the input. We need it otherwise we get a reference to current input
         const input = JSON.parse(JSON.stringify(theData.input));
         const stage = theData.stage;
-        if ( stage === 0 ) // Take care of restart the form after all done (By convention on stage 0 we are restarting)
-            itsHistory = [];
+        if (stage === 0) // Take care of restart the form after all done (By convention on stage 0 we are restarting)
+            this.itsHistory = [];
 
-        const index = itsHistory.length;
-        itsHistory[index] = { "input": input, "stage": stage };
+        const index = this.itsHistory.length;
+        this.itsHistory[index] = { "input": input, "stage": stage };
         // - console.log(`History: Stored stage ${stage} at ${index}`);
     }
 
-    function isHistoryEmpty() {
-        return itsHistory.length <= 1;
+    isHistoryEmpty() {
+        return this.itsHistory.length <= 1;
     }
 
-    function getPreviousStageData() {
-        // We are maintaining the data about all stages as soon as they are rendered.  This means that the
+    getPreviousStageData() {
+        // We are maintaining the data about all stages as soon as they are rendered. This means that the
         // current stage corresponds to the UI currently displayed. We could have stored the stages in history only
         // when moving onto the next stage but it would have complicated the controller as it would have had to maintain
-        // the data about that stage until the next button was clicked.  It is just easier to pop twice to get to the
+        // the data about that stage until the next button was clicked. It is just easier to pop twice to get to the
         // previous stage.
 
         let currentStage;
-        if ( itsHistory.length === 1 ) // Trying to do previous when on the very first step.
-            currentStage = itsHistory[0];
+        if (this.itsHistory.length === 1) // Trying to do previous when on the very first step.
+            currentStage = this.itsHistory[0];
         else
-            currentStage = itsHistory.pop();
+            currentStage = this.itsHistory.pop();
 
-        if ( currentStage === undefined ) {
+        if (currentStage === undefined) {
             console.log('Internal error in history.getPreviousStageData: there should be a current stage');
             return;
         }
 
-        const prevStage = itsHistory.pop();
-        if ( prevStage === undefined ) {
+        const prevStage = this.itsHistory.pop();
+        if (prevStage === undefined) {
             console.log('error in history.getPreviousStageData: there should be a previous stage');
             return;
         }
@@ -51,19 +53,11 @@ corticon.dynForm.History = function () {
         return prevStage;
     }
 
-    function getRestartHistory () {
-        return JSON.stringify(itsHistory);
+    getRestartHistory() {
+        return JSON.stringify(this.itsHistory);
     }
 
-    function setRestartHistory (savedHistory) {
-        itsHistory = JSON.parse(savedHistory);
-    }
-
-    return {
-        setupHistory: setupHistory,
-        getRestartHistory: getRestartHistory,
-        setRestartHistory: setRestartHistory,
-        getPreviousStageData: getPreviousStageData,
-        isHistoryEmpty: isHistoryEmpty
+    setRestartHistory(savedHistory) {
+        this.itsHistory = JSON.parse(savedHistory);
     }
 }
