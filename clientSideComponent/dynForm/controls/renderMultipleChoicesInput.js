b9e8153c1d4e59fd5d6c7cf57e094ed5de0f6a40
Seth Meldon <smeldon@progress.com>
2025-03-22 17:58:27 -0400

    update renderMultipleChoicesInput.js

--- clientSideComponent/dynForm/controls/renderMultipleChoicesInput.js
+++ clientSideComponent/dynForm/controls/renderMultipleChoicesInput.js
@@ -1,11 +1,15 @@
 import { addOptions } from './addOptions.js';
 import { addOptionsFromDataSource } from './addOptionsFromDataSource.js';
 
+export function renderMultipleChoicesInput(oneUIControl, baseEl, labelPositionAtContainerLevel, completionFct) {
+    const inputContainerEl = $('<div class="multipleChoicesContainer"></div>');
+    baseEl.append(inputContainerEl);
 
-if (oneUIControl.options) {
-    addOptions(oneUIControl.options, multipleChoicesEl, oneUIControl);
-}
+    if (oneUIControl.options) {
+        addOptions(oneUIControl.options, inputContainerEl, oneUIControl);
+    }
 
-if (oneUIControl.dataSource) {
-    addOptionsFromDataSource(oneUIControl.dataSource, multipleChoicesEl, inputContainerEl, oneUIControl, completionFct);
+    if (oneUIControl.dataSource) {
+        addOptionsFromDataSource(oneUIControl.dataSource, inputContainerEl, inputContainerEl, oneUIControl, completionFct);
+    }
 }
