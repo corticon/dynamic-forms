d5497bc3e1238c0089d6c71977363d234e82c604
Seth Meldon <smeldon@progress.com>
2025-03-22 17:37:35 -0400

    update createAddExpenseControl.js

--- clientSideComponent/dynForm/controls/createAddExpenseControl.js
+++ clientSideComponent/dynForm/controls/createAddExpenseControl.js
@@ -1,4 +1,6 @@
-function createAddExpenseControl(baseEl, oneUIControl, expenseContainerEl) {
+import { createOneExpenseInput } from './createOneExpenseInput.js';
+
+export function createAddExpenseControl(baseEl, oneUIControl, expenseContainerEl) {
     const html = '<div title="Add another expense" class="addExpenseContainer">&nbsp;+&nbsp;</div>';
     const addContainerEl = $(html);
     baseEl.append(addContainerEl);
