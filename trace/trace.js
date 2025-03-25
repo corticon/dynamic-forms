4e0a7ed75031281e27d33a6fbcc3cc5b1ea7b61e
Seth Meldon <smeldon@progress.com>
2025-03-25 13:03:35 -0400

    update trace.js

--- trace/trace.js
+++ trace/trace.js
@@ -1,15 +1,15 @@
-corticon.util.namespace( "corticon.tracer" );
+corticon.util.namespace("corticon.tracer");
 
-function Tracer () {
+function Tracer() {
     // private section
     let itsStagesTrace = [];
 
     function setupTracing() {
-        corticon.dynForm.addCustomEventHandler( corticon.dynForm.customEvents.BEFORE_START, _clearTraceData );
-        corticon.dynForm.addCustomEventHandler( corticon.dynForm.customEvents.NEW_FORM_DATA_SAVED, _traceFormData);
-        corticon.dynForm.addCustomEventHandler( corticon.dynForm.customEvents.BEFORE_DS_EXECUTION, _traceDecisionServiceInputs);
-        corticon.dynForm.addCustomEventHandler( corticon.dynForm.customEvents.NEW_DS_EXECUTION, _traceDecisionServiceResults);
-        corticon.dynForm.addCustomEventHandler( corticon.tracer.tracerCustomEvents.SWITCH_TO_SAVED_STAGE, _switchToSavedStage);
+        corticon.dynForm.addCustomEventHandler(corticon.dynForm.customEvents.BEFORE_START, _clearTraceData);
+        corticon.dynForm.addCustomEventHandler(corticon.dynForm.customEvents.NEW_FORM_DATA_SAVED, _traceFormData);
+        corticon.dynForm.addCustomEventHandler(corticon.dynForm.customEvents.BEFORE_DS_EXECUTION, _traceDecisionServiceInputs);
+        corticon.dynForm.addCustomEventHandler(corticon.dynForm.customEvents.NEW_DS_EXECUTION, _traceDecisionServiceResults);
+        corticon.dynForm.addCustomEventHandler(corticon.tracer.tracerCustomEvents.SWITCH_TO_SAVED_STAGE, _switchToSavedStage);
     }
 
     function _clearTraceData() {
@@ -21,7 +21,7 @@ function Tracer () {
         $("#traceHistoryId").empty();
     }
 
-    function _traceDecisionServiceInputs ( event ) {
+    function _traceDecisionServiceInputs(event) {
         const theData = event.theData;
         const input = theData.input;
         const stage = theData.stage;
@@ -32,13 +32,13 @@ function Tracer () {
         _addStageInHistory(stage, index);
     }
 
-    function _traceDecisionServiceResults (event) {
+    function _traceDecisionServiceResults(event) {
         const theData = event.theData;
         const result = theData.output;
         const execTimeMs = theData.execTimeMs;
 
         let stageDescription;
-        if ( theData.stageDescription !== undefined && theData.stageDescription !== null )
+        if (theData.stageDescription !== undefined && theData.stageDescription !== null)
             stageDescription = theData.stageDescription;
         else
             stageDescription = "no description provided";
@@ -49,13 +49,13 @@ function Tracer () {
         itsStagesTrace[index].timing = Math.round(execTimeMs);
 
         // add tooltip to the existing node created before we made call to DS
-        const el = $("#traceNodeId_"+index);
+        const el = $("#traceNodeId_" + index);
         el.prop("title", stageDescription);
 
-        _showDecisionServiceResults ( itsStagesTrace[index].result, itsStagesTrace[index].timing );
+        _showDecisionServiceResults(itsStagesTrace[index].result, itsStagesTrace[index].timing);
     }
 
-    function _showDecisionServiceInputs ( newValue ) {
+    function _showDecisionServiceInputs(newValue) {
         document.getElementById("decisionServiceInputId").value = newValue;
     }
 
@@ -67,7 +67,7 @@ function Tracer () {
         _removeHighlightedStage();
 
         let html = `<span>`;
-        if ( index !== 0 )
+        if (index !== 0)
             html += `&rarr;`
 
         html += `<a class="activeStageInTrace stageInTrace" id="traceNodeId_${index}"
@@ -81,6 +81,7 @@ href="#" onclick="corticon.tracer.tracerClickStage(${index}, this)">&nbsp;${stag
 
     function _traceFormData(event) {
         const theData = event.theData;
+        console.log("traceFormData - theData:", theData);
         const index = itsStagesTrace.length - 1;
         const x = JSON.stringify(theData, null, 2);
         itsStagesTrace[index].formData = x;
@@ -98,13 +99,14 @@ href="#" onclick="corticon.tracer.tracerClickStage(${index}, this)">&nbsp;${stag
         _showSavedFormData(oneTrace.formData);
     }
 
-    function _showDecisionServiceResults ( newValue, execTimeMs ) {
+    function _showDecisionServiceResults(newValue, execTimeMs) {
         document.getElementById("decisionServiceResultId").value = newValue;
-        $("#execTimeId").html("(" + execTimeMs +"ms)");
+        $("#execTimeId").html("(" + execTimeMs + "ms)");
     }
 
-    function _showSavedFormData ( newValue ) {
-        if ( newValue === null || newValue.length === 0 )
+    function _showSavedFormData(newValue) {
+        console.log("showSavedFormData - newValue:", newValue);
+        if (newValue === null || newValue.length === 0)
             newValue = "Form Data was not saved at that step";
 
         document.getElementById("formDataId").value = newValue;
@@ -122,7 +124,7 @@ href="#" onclick="corticon.tracer.tracerClickStage(${index}, this)">&nbsp;${stag
         "SWITCH_TO_SAVED_STAGE": "switchToSavedStage",
     }
 
-    corticon.tracer.tracerClickStage = function ( index, theEl ) {
-        corticon.dynForm.raiseEvent(corticon.tracer.tracerCustomEvents.SWITCH_TO_SAVED_STAGE,{ "index": index, "el": theEl });
+    corticon.tracer.tracerClickStage = function (index, theEl) {
+        corticon.dynForm.raiseEvent(corticon.tracer.tracerCustomEvents.SWITCH_TO_SAVED_STAGE, { "index": index, "el": theEl });
     }
 })();
